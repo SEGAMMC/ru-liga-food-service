0b223ad73abd0ed984cc8c7394c51cbdc0df7982
Mukhin Sergey <segam@inbox.ru>
2023-11-05 19:36:14 +0300

    Global refactor migration

--- migration/src/main/resources/db/changelog/v1.0.1/insert-data-customers.sql
+++ migration/src/main/resources/db/changelog/v1.0.1/insert-data-customers.sql
@@ -1,12 +1,12 @@
 INSERT INTO customers (phone, email, address)
-VALUES ('+791565489685', 'timkok@yandex.ru', 'Bor, ul. Lenina, d 8 kv 10'),
-       ('+791125489964', 'ilyamak@yandex.ru', 'Nizhniy Novgorod, ul. Gagarina, d 32 kv 99'),
-       ('+796523478981', 'titovlo@yandex.ru', 'Bor, ul. Lenina, d 84 kv 233'),
-       ('+793678978645', 'raygray@yandex.ru', 'Bor, ul. Lenina, d 23 kv 445'),
-       ('+792354345678', 'fedyakim@yandex.ru', 'Nizhniy Novgorod, ul. Fuchika, d 32 kv 45'),
-       ('+793235476876', 'storaz@yandex.ru', 'Bor, ul. Lenina, d 5 kv 11'),
-       ('+799875544342', 'tihon@yandex.ru', 'Nizhniy Novgorod, ul. Severnaya, d 54 kv 33'),
-       ('+794356765642', 'nikolayper@yandex.ru', 'Nizhniy Novgorod, ul. Novaya, d 1 kv 32'),
-       ('+795467787890', 'niktuchev@yandex.ru', 'Nizhniy Novgorod, ul. Pushkina, d 55 kv 88'),
-       ('+743657879890', 'levpopkov@yandex.ru', 'Bor, ul. Vorovskogo, d 13 kv 14')
+VALUES ('+791565489685', 'timkok@yandex.ru', 'Нижний Новгород, Московское шоссе, 33|56.322472:43.932577'),
+       ('+791125489964', 'ilyamak@yandex.ru', 'Нижний Новгород, улица Бетанкура, 6|56.330346:43.950843'),
+       ('+796523478981', 'titovlo@yandex.ru', 'Нижний Новгород, Ильинская улица, 2|56.328483:43.994736'),
+       ('+793678978645', 'raygray@yandex.ru', 'Нижний Новгород, Грузинская улица, 12В|56.321079:43.996624'),
+       ('+792354345678', 'fedyakim@yandex.ru', 'Нижний Новгород, Ошарская улица, 11Д|56.321442:44.007493'),
+       ('+793235476876', 'storaz@yandex.ru', 'Нижний Новгород, Холодный переулок, 10|56.319442:44.001463'),
+       ('+799875544342', 'tihon@yandex.ru', 'Нижний Новгород, улица Звездинка, 11|56.316151:44.001077'),
+       ('+794356765642', 'nikolayper@yandex.ru', 'Нижний Новгород, Новая улица, 51|56.314219:43.989061'),
+       ('+795467787890', 'niktuchev@yandex.ru', 'Нижний Новгород, Ярославская улица, 21|56.318929:43.977838'),
+       ('+743657879890', 'levpopkov@yandex.ru', 'Нижний Новгород, улица Литвинова, 75|56.315912:43.946566')
 ;
--- migration/src/main/resources/db/changelog/v1.0.1/insert-data-restaurants.sql
+++ migration/src/main/resources/db/changelog/v1.0.1/insert-data-restaurants.sql
@@ -1,12 +1,12 @@
 INSERT INTO restaurants (address, name, status)
-VALUES ('Nizhniy Novgorod, ul. Glavnaya, d 5', 'Shato', 'ACTIVE'),
-       ('Nizhniy Novgorod, ul. Paradnaya, d 12', 'Vasco', 'ACTIVE'),
-       ('Nizhniy Novgorod, ul. Lenina, d 1', 'Vasco', 'ACTIVE'),
-       ('Nizhniy Novgorod, ul. Bolshaya Pokrovskaya, d 113', 'Biblioteka', 'ACTIVE'),
-       ('Nizhniy Novgorod, ul. Minina, d 14', 'Moloko', 'INACTIVE'),
-       ('Nizhniy Novgorod, ul. Rojdestvenskaya, d 25', 'Monte carlo', 'ACTIVE'),
-       ('Nizhniy Novgorod, ul. Baumana, d 32', 'Liberty', 'ACTIVE'),
-       ('Nizhniy Novgorod, ul. Belinskogo, d 4', 'Casto', 'ACTIVE'),
-       ('Nizhniy Novgorod, ul. Artelnaya, d 66', 'Pyat ozer', 'ACTIVE'),
-       ('Nizhniy Novgorod, ul. Kolhoznaya, d 1', 'Druzya', 'INACTIVE')
+VALUES ('Нижний Новгород, Берёзовская улица, 87А|56.329216:43.876094', 'Shato', 'ACTIVE'),
+       ('Нижний Новгород, улица Евгения Никонова, 17|56.331051:43.875585', 'Vasco', 'ACTIVE'),
+       ('Нижний Новгород, улица Красных Зорь, 14А|56.321865:43.873571', 'Vasco', 'ACTIVE'),
+       ('Нижний Новгород, улица Александра Люкина, 7|56.318407:43.878905', 'Biblioteka', 'ACTIVE'),
+       ('Нижний Новгород, Московское шоссе, 30В|56.317239:43.923523', 'Moloko', 'INACTIVE'),
+       ('Нижний Новгород, улица Литвинова, 74Б|56.316194:43.947714', 'Monte carlo', 'ACTIVE'),
+       ('Нижний Новгород, улица Долгополова, 79А|56.313382:43.945229', 'Liberty', 'ACTIVE'),
+       ('Нижний Новгород, бульвар Мира, 9|56.328518:43.955140', 'Casto', 'ACTIVE'),
+       ('Нижний Новгород, Гордеевская улица, 55|56.325386:43.945077', 'Pyat ozer', 'ACTIVE'),
+       ('Нижний Новгород, Гордеевская улица, 1|56.329417:43.939956', 'Druzya', 'INACTIVE')
 ;
